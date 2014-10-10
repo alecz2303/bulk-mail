43edd50b17f4abe3cb47a3c8525e540b726c5ac4
alecz2303 <alecz23@live.com>
2014-10-10 14:58:18 -0500

    Index Actualizado

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+	<link href='http://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
+
 	<!-- If you are using the CSS version, only link these 2 files, you may add app.css to use for your overrides if you like -->
 	  <link rel="stylesheet" href="foundation/css/normalize.css">
 	  <link rel="stylesheet" href="foundation/css/foundation.css">
@@ -11,7 +13,7 @@
 
 
 
-<body>
+<body style="font-family: 'Ubuntu', sans-serif !important">
 <script>
 tinymce.init({
     selector: "textarea#elm1",
@@ -37,14 +39,24 @@ tinymce.init({
 </script>
 
 <script language="javascript">
-	fields = 0;
+	fields = 1;
+	count = 0;
+	var x = [];
+	var y = [];
 	function addInput() {
 		if (fields != 10) {
-			document.getElementById('text').innerHTML += "<div class='large-6 columns'><label>Nombre<input type='text' name='name["+fields+"]' /></label></div><div class='large-6 columns'><label>Correo:<input type='text' name='email["+fields+"]' /></label></div><br />";
+			x.push(document.getElementById("name["+count+"]").value);
+			y.push(document.getElementById("email["+count+"]").value);
+			document.getElementById('text').innerHTML += "<div class='large-6 columns'> <label>Nombre <input required type='text' name='name["+fields+"]' id='name["+fields+"]' /> </label> <small class='error'>Name is required.</small> </div> <div class='large-6 columns'> <label>Correo: <input required type='text' name='email["+fields+"]' id='email["+fields+"]' /> </label> <small class='error'>Email is required.</small> </div> <br />";
+			for(var i = 0; i<x.length; i++){
+				document.getElementById("name["+i+"]").value = x[i];
+				document.getElementById("email["+i+"]").value = y[i];
+			}
 			fields += 1;
+			count += 1;
 		} else {
 			document.getElementById('text').innerHTML += "<br />Only 10 upload fields allowed.";
-			document.form.add.disabled=true;
+			document.getElementById('add').disabled=true;
 		}
 	}
 </script>
@@ -53,19 +65,21 @@ tinymce.init({
 	<div class="text-center">
 		<h1>Bulk Mail</h1>
 	</div>
-	<form method="post">
+	<form method="post" data-abide>
 		<div class="row">
 			<div class="large-6 columns">
 				<label>
 					Mandrill User Name:
-					<input type="text" name="username" />
+					<input required type="text" name="username" />
 				</label>
+				<small class="error">Mandrill User Name is required.</small>
 			</div>
 			<div class="large-6 columns">
 				<label>
 					Mandrill API Key:
-					<input type="text" name="apikey" />
+					<input required	 type="text" name="apikey" />
 				</label>
+				<small class="error">Mandrill API Key is required.</small>
 			</div>
 		</div>
 		<div class="row">
@@ -92,8 +106,19 @@ tinymce.init({
 				</label>
 			</div>
     		<div class="large-4 columns">
-				<input class="button" type="button" onclick="addInput()" name="add" value="Add Mail" />
-				<div id="text"></div>
+				<input id="add" class="button" type="button" onclick="addInput()" name="add" value="Add Mail" />
+				<div id="text">
+					<div class='large-6 columns'>
+						<label>Nombre
+							<input type='text' name='name[0]' id='name[0]' />
+						</label>
+					</div>
+					<div class='large-6 columns'>
+						<label>Correo:
+							<input type='text' name='email[0]' id='email[0]' />
+						</label>
+					</div>
+				</div>
 			</div>
     		<div class="large-8 columns">
     			<textarea id="elm1" name="area"></textarea>
@@ -148,6 +173,5 @@ if(isset($_POST['area'])){
 					->addPart(strip_tags($content), 'text/plain');
 
 		$result = $swift->send($message);
-
 	}
 }
